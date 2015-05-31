c08bdba606c0bea0d43032fb52d57d2bf2c0aee2
Павел Фролов <plfrlv@gmail.com>
2015-05-31 13:37:59 +0300

    Переделали таблицу, иконки

--- src/dev/assets/scripts/classwork.js
+++
@@ -1,4 +0,0 @@
-function hasclass ( el, classname ) {
-  var result = el.classList ? el.classList.contains(classname) : new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
-  return result;
-};
